5f60ab02c6044176e06ada43646749c82e006a40
Matthew.Sammarco <sammarco7167@gmail.com>
2013-07-09 13:38:47 +0000

    fix for web.js remember to jshint!

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    contents = fs.readFileSync('index.html')
+    contents = fs.readFileSync('index.html');
     response.send(contents.toString('utf-8'));
 });
 
